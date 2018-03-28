1cad6e37b09725741114c9ae874f1ccbc56dc114
kdiaz <kerycdiaz@gmail.com>
2018-03-28 13:52:03 -0500

    Running django-starter after postgresql

--- web/server/entrypoint.sh
+++ web/server/entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+while $(echo "from django.db import connections
+from django.db.utils import OperationalError
+db_conn = connections['default']
+try:
+    c = db_conn.cursor()
+except OperationalError:
+    connected = 'true'
+else:
+    connected = 'false'
+print(connected)" | python manage.py shell); do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 5
+done
+
+>&2 echo "Postgres is up - executing command"
+
 echo "[run] go to project folder"
 cd /home/app/server
 

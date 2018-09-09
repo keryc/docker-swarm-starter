a1e0fd39fa96024a96e0ae78bede7c0b137fcd21
Keryc Diaz <kerycdiaz@gmail.com>
2018-09-08 20:33:46 -0500

    remove .git in angular update

--- update-angular-starter.sh
+++ update-angular-starter.sh
@@ -4,7 +4,8 @@ mv web/client/Dockerfile existing-angular/Dockerfile
 rm -R web/client
 
 sudo npm install -g @angular/cli
-cd web; ng new client; cd ../
+cd web; ng new client
+cd client; sudo rm -R .git; cd ../../
 
 cp -R existing-angular/Dockerfile web/client/Dockerfile
 
